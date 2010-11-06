51acfdbca2321fe9f5178f0025b4f1cf4e1f3207
James Forshaw <tyranid@gmail.com>
2010-11-06 14:36:28 +0000

    Added GPLv3 license

--- OleViewDotNet/PowerShellInstance.cs
+++ OleViewDotNet/PowerShellInstance.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Runtime.InteropServices;
+﻿//    This file is part of OleViewDotNet.
+//
+//    OleViewDotNet is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OleViewDotNet is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OleViewDotNet.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Management.Automation;
-using System.Management.Automation.Host;
 using System.Management.Automation.Runspaces;
-using Microsoft.PowerShell;
-using System.Windows.Forms;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.PowerShell;
 
 namespace OleViewDotNet
 {
