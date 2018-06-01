cdb2b2dd4915c1c1876026c4ee0eb2296d3b7397
James Forshaw <forshaw@google.com>
2018-06-01 13:00:37 +0100

    Added PPL protection level.

--- OleViewDotNet/COMAppIDEntry.cs
+++ OleViewDotNet/COMAppIDEntry.cs
@@ -15,15 +15,14 @@
 //    along with OleViewDotNet.  If not, see <http://www.gnu.org/licenses/>.
 
 using Microsoft.Win32;
-using System;
-using System.Linq;
-using System.ComponentModel;
-using System.Xml;
-using System.Xml.Serialization;
-using System.Xml.Schema;
-using System.ServiceProcess;
-using System.Collections.Generic;
 using NtApiDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace OleViewDotNet
 {
@@ -52,6 +51,14 @@ namespace OleViewDotNet
         AllowAnyClient = 1,
     }
 
+    public enum ServiceProtectionLevel
+    {
+        None = 0,
+        Windows = 1,
+        WindowsLight = 2,
+        AntimalwareLight = 3
+    }
+
     public class COMAppIDServiceEntry : IXmlSerializable
     {
         public string DisplayName { get; private set; }
@@ -60,6 +67,7 @@ namespace OleViewDotNet
         public string UserName { get; private set; }
         public string ImagePath { get; private set; }
         public string ServiceDll { get; private set; }
+        public ServiceProtectionLevel ProtectionLevel { get; private set; }
 
         internal COMAppIDServiceEntry(RegistryKey key, 
             ServiceController service)
@@ -79,6 +87,7 @@ namespace OleViewDotNet
                 {
                     ServiceDll = COMUtilities.ReadStringFromKey(key, null, "ServiceDll");
                 }
+                ProtectionLevel = (ServiceProtectionLevel) COMUtilities.ReadIntFromKey(key, null, "LaunchProtected");
             }
         }
 
@@ -95,10 +104,11 @@ namespace OleViewDotNet
         {
             DisplayName = reader.ReadString("display");
             Name = reader.ReadString("name");
-            ServiceType = reader.ReadEnum<System.ServiceProcess.ServiceType>("type");
+            ServiceType = reader.ReadEnum<ServiceType>("type");
             UserName = reader.ReadString("user");
             ImagePath = reader.ReadString("path");
             ServiceDll = reader.ReadString("dll");
+            ProtectionLevel = reader.ReadEnum<ServiceProtectionLevel>("prot");
         }
 
         void IXmlSerializable.WriteXml(XmlWriter writer)
@@ -109,6 +119,7 @@ namespace OleViewDotNet
             writer.WriteOptionalAttributeString("user", UserName);
             writer.WriteOptionalAttributeString("path", ImagePath);
             writer.WriteOptionalAttributeString("dll", ServiceDll);
+            writer.WriteEnum("prot", ProtectionLevel);
         }
 
         public override bool Equals(object obj)
@@ -124,12 +135,12 @@ namespace OleViewDotNet
                 return false;
             }
 
-            return DisplayName == right.DisplayName && Name == right.Name && ServiceType == right.ServiceType && UserName == right.UserName;
+            return DisplayName == right.DisplayName && Name == right.Name && ServiceType == right.ServiceType && UserName == right.UserName && ProtectionLevel == right.ProtectionLevel;
         }
 
         public override int GetHashCode()
         {
-            return DisplayName.GetSafeHashCode() ^ Name.GetSafeHashCode() ^ ServiceType.GetHashCode() ^ UserName.GetSafeHashCode();
+            return DisplayName.GetSafeHashCode() ^ Name.GetSafeHashCode() ^ ServiceType.GetHashCode() ^ UserName.GetSafeHashCode() ^ ProtectionLevel.GetHashCode();
         }
 
         public override string ToString()
@@ -244,6 +255,18 @@ namespace OleViewDotNet
         public bool IsService
         {
             get { return LocalService != null; }
+        }
+
+        public ServiceProtectionLevel ProtectionLevel
+        {
+            get
+            {
+                if (IsService)
+                {
+                    return LocalService.ProtectionLevel;
+                }
+                return ServiceProtectionLevel.None;
+            }
         }
 
         public string LaunchPermission
