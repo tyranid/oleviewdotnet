748c9f920794ec1cb92c29f31925881a6362790d
James Forshaw <tyranid@gmail.com>
2018-09-09 19:40:47 +0100

    Cleanup for assembly information.

--- OleViewDotNet.Main/Properties/AssemblyInfo.cs
+++ OleViewDotNet.Main/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("OleViewDotNet")]
 [assembly: AssemblyDescription("COM/OLE Viewer in .NET")]
 [assembly: AssemblyConfiguration("")]
@@ -13,12 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("42d50bbd-cbed-42c5-98c3-707174b413ac")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
--- OleViewDotNet.PowerShell/Properties/AssemblyInfo.cs
+++ OleViewDotNet.PowerShell/Properties/AssemblyInfo.cs
@@ -1,13 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("OleViewDotNet")]
 [assembly: AssemblyDescription("COM/OLE Viewer in .NET")]
 [assembly: AssemblyConfiguration("")]
@@ -17,12 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ae69dc09-ef09-455c-8779-3a705e5be205")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
