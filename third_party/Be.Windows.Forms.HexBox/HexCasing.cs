0c6f343b874329959f440e9c351a37a378bddc86
James Forshaw <tyranid@users.noreply.github.com>
2023-12-05 16:38:14 -0800

    Added code for hexbox.

---
+++ third_party/Be.Windows.Forms.HexBox/HexCasing.cs
@@ -0,0 +1,17 @@
+﻿namespace Be.Windows.Forms
+{
+    /// <summary>
+    /// Specifies the case of hex characters in the HexBox control
+    /// </summary>
+    public enum HexCasing
+    {
+        /// <summary>
+        /// Converts all characters to uppercase.
+        /// </summary>
+        Upper = 0,
+        /// <summary>
+        /// Converts all characters to lowercase.
+        /// </summary>
+        Lower = 1
+    }
+}
