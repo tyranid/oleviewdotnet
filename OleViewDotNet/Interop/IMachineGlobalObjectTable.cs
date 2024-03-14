25e05c6f8ebd23aead343d8cb786e82a0ac4ffe6
James Forshaw <tyranid@users.noreply.github.com>
2024-03-14 12:54:57 -0700

    Added interfaces for MGOT.

---
+++ OleViewDotNet/Interop/IMachineGlobalObjectTable.cs
@@ -0,0 +1,39 @@
+﻿//    This file is part of OleViewDotNet.
+//    Copyright (C) James Forshaw 2014, 2016
+//
+//    OleViewDotNet is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OleViewDotNet is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OleViewDotNet.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace OleViewDotNet.Interop;
+
+[Guid("26d709ac-f70b-4421-a96f-d2878fafb00d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IMachineGlobalObjectTable
+{
+    IntPtr RegisterObject(
+        in Guid clsid,
+        [MarshalAs(UnmanagedType.LPWStr)]
+        string identifier,
+        [MarshalAs(UnmanagedType.IUnknown)] object obj);
+
+    [return: MarshalAs(UnmanagedType.IUnknown)]
+    object GetObject(
+       in Guid clsid,
+       [MarshalAs(UnmanagedType.LPWStr)]
+       string identifier,
+       in Guid riid);
+
+    void RevokeObject(IntPtr token);
+}
---
+++ OleViewDotNet/Interop/IMachineGlobalObjectTableLegacy.cs
@@ -0,0 +1,43 @@
+﻿//    This file is part of OleViewDotNet.
+//    Copyright (C) James Forshaw 2014, 2016
+//
+//    OleViewDotNet is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OleViewDotNet is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OleViewDotNet.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace OleViewDotNet.Interop;
+
+[Guid("0113f843-62de-462a-a6a8-99d11546034f"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IMachineGlobalObjectTableLegacy
+{
+    IntPtr RegisterObject(
+        MGOTFLAGS flags,
+        in Guid clsid,
+        [MarshalAs(UnmanagedType.HString)]
+        string identifier,
+        [MarshalAs(UnmanagedType.IUnknown)] object obj);
+
+    IList<string> EnumerateObjectIdentifiers(in Guid clsid);
+
+    [return: MarshalAs(UnmanagedType.IUnknown)]
+    object GetObject(
+       in Guid clsid,
+       [MarshalAs(UnmanagedType.HString)]
+       string identifier,
+       in Guid riid);
+
+    void RevokeObject(IntPtr token);
+}
